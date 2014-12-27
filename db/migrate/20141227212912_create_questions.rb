86145502a56f191b3cb1f1962d22335c6b81539c
Prelang Builder <builder@prelang.com>
2014-12-27 21:29:12 +0000

    Generated model Question with migration

---
+++ db/migrate/20141227212912_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.references :lesson, index: true
+      t.text :question_type
+
+      t.timestamps
+    end
+  end
+end
